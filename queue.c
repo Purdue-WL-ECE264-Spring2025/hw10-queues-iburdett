5f2e9f80fdc43bc077641454b6ff7d3ef5669f32
Isaac Matthew Burdette <iburdett@ececomp4.ecn.purdue.edu>
2025-04-04 15:40:51 -0400

    idk

--- queue.c
+++ queue.c
@@ -29,42 +29,96 @@ int number_of_moves(struct game_state start)
     used[startpt % SIZE] = startpt; //used to change startpt to depth inside array 
     enqueue(&q, start); 
 
-    while(q.data.head != NULL)
-    {
-        struct game_state current = dequeue(&q); 
-        int end = 1; 
-        int tile_r = 1;
-
-        for(int i = 0; i < 4 && (end != 0); i++)
+        while(q.data.head != NULL)
         {
-            for(int j = 0; j < 4 && (end != 0); j++)
-            {
-                int tile = current.tiles[i][j];
+            struct game_state current = dequeue(&q); 
+            int end = 1; 
+            int tile_r = 1;
 
-                if (i == 3 && j == 3) 
+            for(int i = 0; i < 4 && (end != 0); i++)
+            {
+                for(int j = 0; j < 4 && (end != 0); j++)
                 {
-                    if (tile != 0)
-                    { 
-                        end = 0;
-                    }
-                    
-                }
-                else 
-                {
-                    if (tile != tile_r++) 
+                    int tile = current.tiles[i][j];
+
+                    if (i == 3 && j == 3) 
                     {
-                        end = 0;
+                        if (tile != 0)
+                        { 
+                            end = 0;
+                        }
+                        
+                    }
+                    else 
+                    {
+                        if (tile != tile_r++) 
+                        {
+                            end = 0;
+                        }
                     }
                 }
             }
-        }
-        if (end) 
-        {
+            
+            if (end) 
+            {
             free_list(q.data);
             free(used);
             return current.num_steps;
+            }
+            for(int i = 0; i < 4; i++)
+            {
+                struct game_state next_pos = current;
+        
+                if(i ==0)
+                {
+                    move_up(&next_pos);
+                }
+        
+                if(i == 1)
+                {
+                    move_down(&next_pos);
+                }
+        
+                if(i ==2)
+                {
+                    move_left(&next_pos);
+                }
+        
+                if(i == 3)
+                {
+                    move_right(&next_pos);
+                }
+        
+                if(next_pos.num_steps != current.num_steps)
+                {
+                    uint64_t s_state = serialize(next_pos);
+                    size_t location = s_state % SIZE; 
+                    int num_seen = 0; 
+                    int target = 0; 
+        
+                    for(int j = 0; j < SIZE && !(num_seen || target); j++)
+                    {
+                        size_t tester = (location + j) % SIZE; 
+        
+                        if(used[tester] == 0)
+                        {
+                            used[tester] = s_state;
+                            target = 1; 
+                        }
+        
+                        else if(used[tester] == s_state)
+                        {
+                            num_seen = 1; 
+                        }
+                    }
+        
+                    if(!num_seen)
+                    {
+                        enqueue(&q, next_pos);
+                    }
+                } 
+            }
         }
-    }
 
     // if (end) 
     // {
@@ -73,59 +127,59 @@ int number_of_moves(struct game_state start)
     //     return current.num_steps;
     // }
 
-    for(int i = 0; i < 4; i++)
-    {
-        struct game_state next_pos = current;
+    // for(int i = 0; i < 4; i++)
+    // {
+    //     struct game_state next_pos = current;
 
-        if(i ==0)
-        {
-            move_up(&next_pos);
-        }
+    //     if(i ==0)
+    //     {
+    //         move_up(&next_pos);
+    //     }
 
-        if(i == 1)
-        {
-            move_down(&next_pos);
-        }
+    //     if(i == 1)
+    //     {
+    //         move_down(&next_pos);
+    //     }
 
-        if(i ==2)
-        {
-            move_left(&next_pos);
-        }
+    //     if(i ==2)
+    //     {
+    //         move_left(&next_pos);
+    //     }
 
-        if(i == 3)
-        {
-            move_right(&next_pos);
-        }
+    //     if(i == 3)
+    //     {
+    //         move_right(&next_pos);
+    //     }
 
-        if(next_pos.num_steps != current.num_steps)
-        {
-            uint64_t s_state = serialize(next_pos);
-            size_t location = s_state % SIZE; 
-            int num_seen = 0; 
-            int target = 0; 
+    //     if(next_pos.num_steps != current.num_steps)
+    //     {
+    //         uint64_t s_state = serialize(next_pos);
+    //         size_t location = s_state % SIZE; 
+    //         int num_seen = 0; 
+    //         int target = 0; 
 
-            for(int j = 0; j < SIZE && !(num_seen || target); j++)
-            {
-                size_t tester = (location + j) % SIZE; 
+    //         for(int j = 0; j < SIZE && !(num_seen || target); j++)
+    //         {
+    //             size_t tester = (location + j) % SIZE; 
 
-                if(used[tester] == 0)
-                {
-                    used[tester] = s_state;
-                    target = 1; 
-                }
+    //             if(used[tester] == 0)
+    //             {
+    //                 used[tester] = s_state;
+    //                 target = 1; 
+    //             }
 
-                else if(used[tester] == s_state)
-                {
-                    num_seen = 1; 
-                }
-            }
+    //             else if(used[tester] == s_state)
+    //             {
+    //                 num_seen = 1; 
+    //             }
+    //         }
 
-            if(!num_seen)
-            {
-                enqueue(&q, next_pos);
-            }
-        } 
-    }
+    //         if(!num_seen)
+    //         {
+    //             enqueue(&q, next_pos);
+    //         }
+    //     } 
+    // }
 
  free_list(q.data); 
  free(used);
